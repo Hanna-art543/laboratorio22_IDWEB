9883c895f32f6b8f37e07361dc38029990b4caa3
Ana Miranda <amirandalo@unsa.edu.pe>
2025-12-17 14:55:16 -0500

    Subir ejercicio 02

---
+++ ejercicio02.py
@@ -0,0 +1,19 @@
+import requests
+
+def listar_pokemons():
+    
+    params = {"limit": 10}
+    url = "https://pokeapi.co/api/v2/pokemon"
+    
+    respuesta = requests.get(url, params=params)
+    
+    if respuesta.status_code == 200:
+        resultados = respuesta.json().get('results', [])
+        print("Primeros 10 Pokémon")
+        for i, pokemon in enumerate(resultados, 1):
+            print(f"{i}. {pokemon['name']}")
+    else:
+        print("No se pudo obtener la lista.")
+
+if __name__ == "__main__":
+    listar_pokemons()
